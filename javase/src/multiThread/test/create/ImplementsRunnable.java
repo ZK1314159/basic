a982a6db8f236823b52480c486bef782c7f16875
zengkai <979384911@qq.com>
2021-12-07 17:19:17 +0800

    某个线程出现问题并不会导致整个JVM退出

--- javase/src/multiThread/test/create/ImplementsRunnable.java
+++ javase/src/multiThread/test/create/ImplementsRunnable.java
@@ -9,9 +9,13 @@ public class ImplementsRunnable implements Runnable {
 
     @Override
     public void run() {
+        String name = Thread.currentThread().getName();
         for (int i = 0; i < 10; i++) {
-            String name = Thread.currentThread().getName();
+
             System.out.println(name + ": " + i);
+        }
+        if ("Test".equals(name)) {
+            throw new RuntimeException();
         }
         try {
             Thread.sleep(60000);
@@ -22,7 +26,7 @@ public class ImplementsRunnable implements Runnable {
     public static void main(String[] args) {
         Runnable runnable = new ImplementsRunnable();
         Thread thread1 = new Thread(runnable);
-        Thread thread2 = new Thread(runnable);
+        Thread thread2 = new Thread(runnable, "Test");
         Thread thread3 = new Thread(() -> {
             for (int i = 0; i < 10; i++) {
                 String name = Thread.currentThread().getName();
