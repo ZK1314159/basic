17933ef97067849138b0b8905da3d18a962456f0
zengkai <979384911@qq.com>
2021-12-10 08:47:02 +0800

    测试javap指令

---
+++ javase/src/javase/Javap.java
@@ -0,0 +1,11 @@
+package javase;
+
+/**
+ */
+public class Javap {
+    public static void main(String[] args) {
+        Javap javap = new Javap();
+        // javase.Javap
+        // D:\Software\Work\Data\Idea\Projects\java\javase\src\javase\Javap.java
+    }
+}
