1a055960e170667be02454bbdca3aa4b085f6d8e
zengkai <979384911@qq.com>
2021-12-20 11:59:09 +0800

    交替打印100

---
+++ javase/src/multiThread/threadcommunication/interprint/PrintA.java
@@ -0,0 +1,35 @@
+package multiThread.threadcommunication.interprint;
+
+/**
+ * Description：<br>
+ * <br>
+ * CreateDate：2021/12/20 11:01 <br>
+ */
+public class PrintA implements Runnable {
+
+    private final InterPrintNumber interPrintNumber;
+
+    public PrintA(InterPrintNumber interPrintNumber) {
+        this.interPrintNumber = interPrintNumber;
+    }
+
+    @Override
+    public void run() {
+        synchronized(interPrintNumber) {
+            for (int i = 0; i < 100; i++) {
+                if (interPrintNumber.letAWork) {
+                    System.out.println(Thread.currentThread().getName() + ": " + (i + 1));
+                    interPrintNumber.letAWork = false;
+                    interPrintNumber.notifyAll();
+                } else {
+                    try {
+                        interPrintNumber.wait();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+}
---
+++ javase/src/multiThread/threadcommunication/interprint/PrintB.java
@@ -0,0 +1,35 @@
+package multiThread.threadcommunication.interprint;
+
+/**
+ * Description：<br>
+ * <br>
+ * CreateDate：2021/12/20 11:01 <br>
+ */
+public class PrintB implements Runnable {
+
+    private final InterPrintNumber interPrintNumber;
+
+    public PrintB(InterPrintNumber interPrintNumber) {
+        this.interPrintNumber = interPrintNumber;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 1; i < 100; i++) {
+            synchronized(interPrintNumber) {
+                if (!interPrintNumber.letAWork) {
+                    System.out.println(Thread.currentThread().getName() + ": " + (i + 1));
+                    interPrintNumber.letAWork = true;
+                    interPrintNumber.notifyAll();
+                } else {
+                    try {
+                        interPrintNumber.wait();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+}
