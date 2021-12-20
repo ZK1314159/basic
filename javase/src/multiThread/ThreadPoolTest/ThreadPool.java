22b337a9a69b20fe8453ee27bd707e2a60f5c3d9
zengkai <979384911@qq.com>
2021-12-20 22:28:49 +0800

    ThreadPool Test

---
+++ javase/src/multiThread/ThreadPoolTest/ThreadPool.java
@@ -0,0 +1,34 @@
+package multiThread.ThreadPoolTest;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Description：<br>
+ * <br>
+ * CreateDate：2021/12/20 21:24 <br>
+ */
+public class ThreadPool {
+
+    public static void main(String[] args) throws Exception {
+        ThreadPoolExecutor threadPoolExecutor =
+                new ThreadPoolExecutor(5, 10, 100, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>(10),
+                        new ThreadPoolExecutor.CallerRunsPolicy());
+        Thread thread = new Thread(() -> {
+            try {
+                System.out.println(Thread.currentThread().getName() + " is running...");
+                Thread.sleep(10000);
+                System.out.println(Thread.currentThread().getName() + " finish");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        for (int i = 0; i < 26; i++) {
+            threadPoolExecutor.submit(thread);
+        }
+        System.out.println(Thread.currentThread().getName() + " do work");
+        threadPoolExecutor.shutdown();
+    }
+
+}
--- javase/src/multiThread/ThreadTest.java
+++ javase/src/multiThread/ThreadTest.java
@@ -1,42 +1,9 @@
 package multiThread;
 
-import multiThread.other.Singleton;
-
-import java.util.concurrent.*;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicReference;
-
 class ThreadTest {
+
     public static void main(String[] args) throws Exception {
 
-        AtomicReference<Singleton> atomicReference = new AtomicReference<>();
-//        atomicReference.compareAndSet()
-        AtomicInteger atomicInteger = new AtomicInteger();
-//        atomicInteger.compareAndSet()
-
-        new Thread("新线程1") {
-            public void run() {
-                for (int i = 0; i < 20; i++) {
-                    System.out.println(getName() + " " + i);
-                }
-            }
-        }.start();
-
-        new Thread(() -> {
-            for (int i = 0; i < 20; i++) {
-                System.out.println(Thread.currentThread().getName() + " " + i);
-            }
-        }, "新线程2").start();
-
-        System.out.println("主线程");
-
-        FutureTask<Integer> task = new FutureTask<>(() -> {
-            int j = 0;
-            for (int i = 0; i < 20; i++) {
-                System.out.println(Thread.currentThread().getName() + " " + i);
-            }
-            return j;
-        });
-        new Thread(task, "新线程3").start();
     }
+
 }
