c3d588721725fe485910ba206e49cdc000cc6e9b
zengkai <979384911@qq.com>
2021-12-29 21:11:21 +0800

    动态代理测试

---
+++ designPattern/src/proxy/InvokeHandler.java
@@ -0,0 +1,28 @@
+package proxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+/**
+ * Description：<br>
+ * <br>
+ * CreateDate：2021/12/29 20:12 <br>
+ */
+public class InvokeHandler implements InvocationHandler {
+
+    private NormalService normalService;
+
+    public InvokeHandler(NormalService normalService) {
+        this.normalService = normalService;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        System.out.println("invoke start");
+        System.out.println(method.getName() + args[0]);
+        Object result = method.invoke(normalService, args);
+        System.out.println("invoke finish");
+        return result;
+    }
+
+}
---
+++ designPattern/src/proxy/NormalService.java
@@ -0,0 +1,12 @@
+package proxy;
+
+/**
+ * Description：<br>
+ * <br>
+ * CreateDate：2021/12/29 19:57 <br>
+ */
+public interface NormalService {
+
+    Integer test(Integer id);
+
+}
