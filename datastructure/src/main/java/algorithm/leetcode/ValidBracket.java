751ae83dbf1bcc4abc34492a0da4a18adda8e0cb
zengkai <979384911@qq.com>
2022-02-23 16:58:55 +0800

    leetcode 20 有效的括号,标准解答

---
+++ datastructure/src/main/java/algorithm/leetcode/ValidBracket.java
@@ -0,0 +1,55 @@
+package algorithm.leetcode;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Description：<br>
+ * <br>
+ * CreateDate：2022/2/19 14:15 <br>
+ */
+public class ValidBracket {
+
+    Map<Character, Character> map = new HashMap<>(3);
+
+    public boolean isValid(String s) {
+
+        if (s.length() % 2 == 1) {
+            return false;
+        }
+
+        // 构造映射
+        map.put(')','(');
+        map.put('}','{');
+        map.put(']','[');
+
+        Deque<Character> deque = new ArrayDeque<>();
+        for (int i = 0; i < s.length(); i++) {
+            char tmp = s.charAt(i);
+            if (map.containsKey(tmp)) {
+                if (deque.isEmpty() || deque.poll() != map.get(tmp)) {
+                    return false;
+                }
+            } else {
+                deque.push(tmp);
+            }
+        }
+
+        return deque.isEmpty();
+    }
+
+
+
+    public static void main(String[] args) {
+        Deque<Integer> deque = new ArrayDeque<>();
+        deque.poll();
+        ValidBracket validBracket = new ValidBracket();
+        boolean isValid = validBracket.isValid("([]{}{{}})[][[[]]](())");
+    }
+
+}
