b0bb89b96d956159b5568d709dbdecf97970ddd5
zengkai <979384911@qq.com>
2022-02-28 22:10:19 +0800

    leetcode 102 二叉树层序遍历 优化

--- datastructure/src/main/java/leetcode/BinaryTreeLayerScan.java
+++ datastructure/src/main/java/leetcode/BinaryTreeLayerScan.java
@@ -2,9 +2,9 @@ package leetcode;
 
 import tree.TreeNode;
 
-import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -14,45 +14,31 @@ import java.util.List;
  */
 public class BinaryTreeLayerScan {
 
-    public static class NodeAndLayer {
-        public TreeNode treeNode;
-        public int layer;
-        public NodeAndLayer(TreeNode treeNode, int layer) {
-            this.treeNode = treeNode;
-            this.layer = layer;
-        }
-    }
-
     public List<List<Integer>> levelOrder(TreeNode root) {
         if (root == null) {
             return new ArrayList<>();
         }
 
-        Deque<NodeAndLayer> deque = new ArrayDeque<>();
-        deque.offer(new NodeAndLayer(root, 0));
-        int currentLayer = 0;
+        Deque<TreeNode> deque = new LinkedList<>();
+        deque.offer(root);
         List<List<Integer>> lists = new ArrayList<>();
-        List<Integer> tmp = new ArrayList<>();
         while (! deque.isEmpty()) {
-            NodeAndLayer nodeAndLayer = deque.poll();
-            int layer = nodeAndLayer.layer;
-            TreeNode node = nodeAndLayer.treeNode;
-            if (layer > currentLayer) {
-                lists.add(new ArrayList<>(tmp));
-                tmp.clear();
-                currentLayer = layer;
-                tmp.add(node.val);
-            } else {
-                tmp.add(node.val);
+            int currentSize = deque.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < currentSize; i++) {
+                TreeNode node = deque.poll();
+                if (node != null) {
+                    list.add(node.val);
+                    if (node.left != null) {
+                        deque.offer(node.left);
+                    }
+                    if (node.right != null) {
+                        deque.offer(node.right);
+                    }
+                }
             }
-            if (node.left != null) {
-                deque.offer(new NodeAndLayer(node.left, layer + 1));
-            }
-            if (node.right != null) {
-                deque.offer(new NodeAndLayer(node.right, layer + 1));
-            }
+            lists.add(list);
         }
-        lists.add(tmp);
 
         return lists;
     }
