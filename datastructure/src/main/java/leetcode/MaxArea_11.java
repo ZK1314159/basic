6b96081ab64881194e2ae68dc3b9869e56f0e259
zengkai <979384911@qq.com>
2022-03-03 16:11:19 +0800

    leetcode 11. 盛最多水的容器

---
+++ datastructure/src/main/java/leetcode/MaxArea_11.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+/**
+ * Description：<br>
+ * <br>
+ * CreateDate：2022/3/3 11:56 <br>
+ */
+public class MaxArea_11 {
+
+    public int maxArea(int[] height) {
+        int max = 0;
+        int left = 0;
+        int right = height.length - 1;
+        while (left < right) {
+            int area = (right - left) * Math.min(height[left], height[right]);
+            if (area > max) {
+                max = area;
+            }
+            if (height[left] <= height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return max;
+    }
+
+}
