19ae2c2ad658443332df119db6cca1de61f3c984
zengkai <979384911@qq.com>
2022-03-04 16:20:21 +0800

    leetcode 19. 删除链表的倒数第 N 个结点

---
+++ datastructure/src/main/java/leetcode/RemoveNthFromEnd.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+import historry.exam2022.zijie.ListNode;
+
+/**
+ * Description：<br>
+ * <br>
+ * CreateDate：2022/3/4 15:59 <br>
+ */
+public class RemoveNthFromEnd {
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode start = head;
+        int count = 1;
+        ListNode pre = head;
+        while (head.next != null) {
+            count++;
+            if (count >= n + 2) {
+                pre = pre.next;
+            }
+            head = head.next;
+        }
+
+        if (count == n) {
+            if (count == 1) {
+                return null;
+            }
+            return start.next;
+        }
+        pre.next = pre.next.next;
+
+        return start;
+    }
+
+}
