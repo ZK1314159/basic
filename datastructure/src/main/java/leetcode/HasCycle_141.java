e55cc6be4b8627571bfe9cf736857d935d65c438
zengkai <979384911@qq.com>
2022-03-18 16:33:15 +0800

    leetcode 141. 环形链表

---
+++ datastructure/src/main/java/leetcode/HasCycle_141.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+import historry.exam2022.zijie.ListNode;
+
+/**
+ * Description：<br>
+ * 16:57 想到o(N)空间占用的算法
+ * 17:09 了解了官方的算法
+ * 17:24 官方的算法AC
+ * CreateDate：2022/3/18 16:23 <br>
+ */
+public class HasCycle_141 {
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+            if (fast.next == null) {
+                return false;
+            }
+            fast = fast.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
