a85eae98951127b329c4ce6001a7e8f93ce8625d
zengkai <979384911@qq.com>
2022-03-21 17:49:31 +0800

    leetcode 206. 反转链表

---
+++ datastructure/src/main/java/leetcode/ReverseList_206.java
@@ -0,0 +1,42 @@
+package leetcode;
+
+import historry.exam2022.zijie.ListNode;
+
+/**
+ * Description：<br>
+ * <br>
+ * CreateDate：2022/3/21 17:17 <br>
+ */
+public class ReverseList_206 {
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        ListNode next = head.next;
+        head.next = null;
+        while (next != null) {
+            ListNode tmp = next.next;
+            next.next = head;
+            head = next;
+            next = tmp;
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode listNode1 = new ListNode(1);
+        ListNode listNode2 = new ListNode(2);
+        listNode1.next = listNode2;
+        ListNode listNode3 = new ListNode(3);
+        listNode2.next = listNode3;
+        ListNode listNode4 = new ListNode(4);
+        listNode3.next = listNode4;
+        ListNode listNode5 = new ListNode(5);
+        listNode4.next = listNode5;
+        ReverseList_206 test = new ReverseList_206();
+        ListNode answer = test.reverseList(listNode1);
+    }
+
+
+}
