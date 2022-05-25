3f6cfeafb0074235c708d876a56674afc77af3b1
zengkai <979384911@qq.com>
2022-05-25 19:13:16 +0800

    等待测试

--- javase/src/main/java/multiThread/countdown/CountDownTest.java
+++ javase/src/main/java/multiThread/countdown/CountDownTest.java
@@ -11,10 +11,22 @@ public class CountDownTest {
 
     public static void main(String[] args) throws Exception {
         CountDownLatch countDownLatch = new CountDownLatch(3);
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 3; i++) {
+            int k = i;
             Thread j = new Thread(() -> {
-                System.out.println("subThread: " + Thread.currentThread().getName());
+                System.out.println("subThread: " + Thread.currentThread().getName() + "start");
+                int start = (int) System.currentTimeMillis() / 1000;
+                if (k == 2) {
+                    try {
+                        Thread.sleep(5000);
+                    } catch (Exception e) {
+
+                    }
+                }
                 countDownLatch.countDown();
+                int end = (int) System.currentTimeMillis() / 1000;
+                System.out.println("subThread: " + Thread.currentThread().getName() + "finish" + "耗时" +
+                        (end - start) + "ms");
             });
             j.start();
         }
--- javase/src/main/java/multiThread/cyclicbarrier/CyclicBarrierTest.java
+++ javase/src/main/java/multiThread/cyclicbarrier/CyclicBarrierTest.java
@@ -14,11 +14,22 @@ public class CyclicBarrierTest {
         CyclicBarrier cyclicBarrier = new CyclicBarrier(4, () -> {
             System.out.println("all thread finish");
         });
-        for (int i = 0; i < 8; i++) {
+        for (int i = 0; i < 4; i++) {
+            int j = i;
             new Thread(() -> {
                 try {
+                    int start = (int) System.currentTimeMillis() / 1000;
                     System.out.println(Thread.currentThread().getName() + ": waiting");
+                    if (j == 3) {
+                        try {
+                            Thread.sleep(5000);
+                        } catch (Exception e) {
+
+                        }
+                    }
                     cyclicBarrier.await();
+                    int end = (int) System.currentTimeMillis() / 1000;
+                    System.out.println(Thread.currentThread().getName() + "finish, " + "耗时" + (end - start) + "ms");
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
