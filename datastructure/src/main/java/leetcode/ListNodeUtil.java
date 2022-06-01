e04b024b8339b100299eb4af65add7aae734b99e
zengkai <979384911@qq.com>
2022-06-01 15:32:09 +0800

    工具类

---
+++ datastructure/src/main/java/leetcode/ListNodeUtil.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+import historry.exam2022.zijie.ListNode;
+
+/**
+ * Description：<br>
+ * <br>
+ * CreateDate：2022/5/30 23:15 <br>
+ */
+public class ListNodeUtil {
+
+    public static ListNode getListNode(int[] array) {
+        if (array == null || array.length == 0) {
+            return null;
+        }
+        if (array.length == 1) {
+            return new ListNode(array[0]);
+        }
+        ListNode head = new ListNode(array[0]);
+        ListNode tail = head;
+        for (int i = 1; i < array.length; i++) {
+            tail.next = new ListNode(array[i]);
+            tail = tail.next;
+        }
+        return head;
+    }
+
+}
