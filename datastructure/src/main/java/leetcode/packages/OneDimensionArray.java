04bd7670e635ff7df83bc8af6647f9fbacc75fea
zengkai <979384911@qq.com>
2022-06-08 16:52:15 +0800

    01背包问题

---
+++ datastructure/src/main/java/leetcode/packages/OneDimensionArray.java
@@ -0,0 +1,32 @@
+package leetcode.packages;
+
+/**
+ * Description：<br>
+ * <br>
+ * CreateDate：2022/6/8 15:46 <br>
+ */
+public class OneDimensionArray {
+
+    public int maxValue(int[][] goods, int packageSize) {
+        int[] dp = new int[packageSize + 1];
+        for (int i = 0; i < goods.length; i++) {
+            for (int j = packageSize; j > 0; j--) {
+                int weight = goods[i][0];
+                int value = goods[i][1];
+                if (weight <= j) {
+                    int candidate = dp[j - weight] + value;
+                    dp[j] = Math.max(dp[j], candidate);
+                }
+            }
+        }
+        return dp[packageSize];
+    }
+
+    public static void main(String[] args) {
+        int[][] goods = new int[][] {{1, 15}, {3, 20}, {4, 30}};
+        int packageSize = 4;
+        OneDimensionArray test = new OneDimensionArray();
+        int answer = test.maxValue(goods, packageSize);
+    }
+
+}
---
+++ datastructure/src/main/java/leetcode/packages/TwoDimensionArray.java
@@ -0,0 +1,39 @@
+package leetcode.packages;
+
+/**
+ * Description：<br>
+ * <br>
+ * CreateDate：2022/6/8 15:46 <br>
+ */
+public class TwoDimensionArray {
+
+    public int maxValue(int[][] goods, int packageSize) {
+        int number = goods.length;
+        int[][] dp = new int[number + 1][packageSize + 1];
+        for (int i = 0; i <= number; i++) {
+            for (int j = 0; j <= packageSize; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                    continue;
+                }
+                int weight = goods[i - 1][0];
+                int value = goods[i - 1][1];
+                if (weight > j) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    int candidate = dp[i - 1][j - weight] + value;
+                    dp[i][j] = Math.max(dp[i - 1][j], candidate);
+                }
+            }
+        }
+        return dp[number][packageSize];
+    }
+
+    public static void main(String[] args) {
+        int[][] goods = new int[][] {{1, 15}, {3, 20}, {4, 30}};
+        int packageSize = 4;
+        TwoDimensionArray test = new TwoDimensionArray();
+        int answer = test.maxValue(goods, packageSize);
+    }
+
+}
